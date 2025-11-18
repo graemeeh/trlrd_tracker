f1e15d9e9acc599d21aedd4e2468d8eccb4b5050
graemeeh <graeme.eh@gmail.com>
2025-11-18 13:39:10 -0800

    Update background_utilities.py

--- background_utilities.py
+++ background_utilities.py
@@ -18,14 +18,15 @@ def chunks(n, min, thresh):
         min += 1
     return min
 
-def play_vid_t(M, h, w, index, scale, path, title="Video"):
+def play_vid_t(M, h, w, index, scale, path, out, title="Video"):
     vid = cv2.VideoCapture(path)
     df_l = []
     paused = False
     for i in range(M.shape[2]):
         frame = M[:,:, i]
         f1 = cv2.resize(np.uint8(cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)), (int(w / scale), int(h / scale)))
-        frame = cv2.adaptiveThreshold(f1, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, (M.shape[0]//16 if (M.shape[0]//16) % 2 ==1 else M.shape[0]//16 + 1), (-M.shape[0]//32 if (M.shape[0]//32) % 2 ==1 else -M.shape[0]//32 + 1))
+        frame = cv2.adaptiveThreshold(f1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, (M.shape[0]//32 if (M.shape[0]//32) % 2 ==1 else M.shape[0]//32 + 1), (-M.shape[0]//64 if (M.shape[0]//64) % 2 ==1 else -M.shape[0]//64 + 1))
+        out.write(frame)
         df, worms = worms_track(frame, min=150, max=1000, time=i, ratio = 1)
         df_l.append(df)
         f = cv2.resize(worms, (1024, 768))
@@ -33,8 +34,8 @@ def play_vid_t(M, h, w, index, scale, path, title="Video"):
         _, b = vid.read()
         b = cv2.resize(b, (1024, 768))
         f = cv2.merge((f, np.zeros_like(f), f))
-        cv2.imshow(title, cv2.addWeighted(b, 0.5, f, 0.9, 0))
-        key = cv2.waitKey(300) & 0xFF
+        cv2.imshow(title, cv2.addWeighted(b, 0.5, f, 0, 0))
+        key = cv2.waitKey(1000) & 0xFF
         if key == ord('q'):
             break
         elif key == ord(' '):
