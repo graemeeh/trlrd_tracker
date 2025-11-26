e83451f1a7f56d1396d08321689be6f14927d0cf
graemeeh <graeme.eh@gmail.com>
2025-11-25 17:26:19 -0800

    Update tracker_utilities.py

--- tracker_utilities.py
+++ tracker_utilities.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 import pandas as pd
 import scipy
-import os
 
 from cv2 import COLOR_GRAY2BGR
 
@@ -10,17 +9,15 @@ from cv2 import COLOR_GRAY2BGR
 def log_abs(img, sigma: float = 0.3):
     """
     see https://www.youtube.com/watch?v=uNP6ZwQ3r6A for how this works
-    :param img:
-    :param sigma:
-    :param thresh:
-    :return:
+    :param img: input frame from video
+    :param sigma: standard deviation of gaussian
+    :return: laplacian of gaussian of frame, highlights edges
     """
     img = cv2.Laplacian(cv2.GaussianBlur(img, (0, 0), sigma), cv2.CV_64F) # note data type cv_64f
     return cv2.normalize(np.abs(-img), None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8) # gets absolute value & scales the pixel values of array so that theyare between 0-255
 
 def worms_track(edges, min: int, max: int, ratio: float, time: int):
     """
-
     :param edges: input image
     :param min: minimum area
     :param max: max area
@@ -53,7 +50,7 @@ def worms_track(edges, min: int, max: int, ratio: float, time: int):
 
 def median_frame(vid):
     # This is from https://github.com/samwestby/OpenCV-Python-Tutorial/blob/main/8_background_est.py
-    frame_ids = [i for i in range(1, 41)]
+    frame_ids = [i for i in range(0, 41)]
     frames = []
     for f in frame_ids:
         vid.set(cv2.CAP_PROP_POS_FRAMES, f)
@@ -64,16 +61,42 @@ def median_frame(vid):
         frames.append(f)
     return cv2.cvtColor(np.median(frames, axis=0).astype(np.uint8), cv2.COLOR_BGR2GRAY)
 
+def mode_frame(vid, r):
+    # This is from https://github.com/samwestby/OpenCV-Python-Tutorial/blob/main/8_background_est.py but made to grab the mode
+    from scipy import stats
+    frame_ids = [i for i in range(0, r)]
+    frames = []
+    for f in frame_ids:
+        vid.set(cv2.CAP_PROP_POS_FRAMES, f)
+        r, f = vid.read()
+        if not r:
+            print("SOMETHING WENT WRONG!!!!")
+            exit()
+        frames.append(f)
+    return cv2.cvtColor(stats.mode(frames, axis=0).astype(np.uint8), cv2.COLOR_BGR2GRAY)
+
 def resizr(image, scale):
+    """
+    replacement for scipy.misc.imresize
+    :param image: input frame
+    :param scale: how big relative to original the image should be
+    :return: resized image
+    """
     h, w = image.shape[:2]
     return cv2.resize(image, (int(w * scale), int(h * scale)), interpolation=cv2.INTER_CUBIC)
 
 def vid_setup(c, s):
+    """
+
+    :param c: video capture cv2
+    :param s: resizr resizing factor
+    :return: number of frames, height and width of frame
+    """
     ret, frame = c.read()
     n_frames = int(c.get(cv2.CAP_PROP_FRAME_COUNT))
     gray = resizr(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), s)
     h, w = gray.shape[:2]
-    return n_frames, h, w
+    return n_frames, h, w, gray
 
 def feats_detect(vid, min: int = 10000, max: int = 10000000, ksize: int = 155):
     """
@@ -94,12 +117,13 @@ def feats_detect(vid, min: int = 10000, max: int = 10000000, ksize: int = 155):
     circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, minDis, param1=14, param2=100, minRadius=minR, maxRadius=maxR)
     details = []
     '''
-    lines = cv2.HoughLinesP(blur, rho = 1, theta = np.pi / 180, threshold = 500, lines = np.array([]), minLineLength = 500, maxLineGap = 20 )
+    lines = cv2.HoughLinesP(blur_gray3, rho = 1, theta = np.pi / 180, threshold = 500, lines = np.array([]), minLineLength = 500, maxLineGap = 20 )
     for line in lines:
         for x1, y1, x2, y2 in line:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
             details.append({'type': 'line','x1': x1,'y1': y1,'x2/radius': x2,'y2': y2})
     '''
+    # circles = cv2.HoughCircles(blur_gray3, cv2.HOUGH_GRADIENT, 2, blur_gray3.shape[0]/10, param1=200, param2=600, minRadius=100, maxRadius= int(blur_gray3.shape[0]/1.5))
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
@@ -122,11 +146,3 @@ def join_tracks(f: pd.DataFrame, maxdist: int):
         # optimize distance between tracks in this frame and tracks in the following frame. Join tracks by updating indices in the following frame to match the joined tracks.
         # maybe also optimize area difference between subsequent frames? cost matrix from https://github.com/Tierpsy/tierpsy-tracker/blob/development/tierpsy/analysis/traj_join/joinBlobsTrajectories.py
     return f
-
-VIDEO = cv2.VideoCapture("N2.avi")
-d, b = feats_detect(VIDEO)
-while True:
-    b = cv2.resize(b, (1024, 768))
-    cv2.imshow('Video', b)
-    if cv2.waitKey(1000) & 0xFF == ord('q'):
-        break
