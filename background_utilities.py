b0b438f64d5e9f47dc160756a084d6cc59edfe12
graemeeh <graeme.eh@gmail.com>
2026-07-03 14:58:05 -0700

    Update background_utilities.py
    ignore output files

--- background_utilities.py
+++ background_utilities.py
@@ -17,12 +17,12 @@ def shrink(X, tau):
 def grab_vids(folder):
     """
     :param folder: input folder that you want to recursively track all .avi files in
-    :return: all videos (with .avi extension) in a given folder
+    :return: all videos (with .avi extension) in a given folder, EXCLUDING output files
     """
     vids = []
     for root, dirs, files in os.walk(folder):
         for f in files:
-            if f.endswith('.avi'):
+            if f.endswith('.avi') and not f.endswith('output.avi'):
                 vids.append(os.path.join(root, f))
     return vids
 
@@ -57,15 +57,15 @@ def play_vid_t(M, h, w, index, scale, path, out, title="Video"):
         f1 = cv2.resize(np.uint8(cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)), (int(w / scale), int(h / scale)))
         frame = cv2.adaptiveThreshold(f1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, (M.shape[0]//32 if (M.shape[0]//32) % 2 ==1 else M.shape[0]//32 + 1), (-M.shape[0]//64 if (M.shape[0]//64) % 2 ==1 else -M.shape[0]//64 + 1))
         out.write(frame)
-        df, worms = worms_track(frame, min=150, max=1000, time=i, ratio = 1)
+        df, worms = worms_track(frame, min=100, max=1000, time=i, ratio = 1)
         df_l.append(df)
         f = cv2.resize(worms, (1024, 768))
         vid.set(cv2.CAP_PROP_POS_FRAMES, i + index)
         _, b = vid.read()
         b = cv2.resize(b, (1024, 768))
         f = cv2.merge((f, np.zeros_like(f), f))
-        cv2.imshow(title, cv2.addWeighted(b, 0.5, f, 0, 0))
-        key = cv2.waitKey(1000) & 0xFF
+        cv2.imshow(title, cv2.addWeighted(b, 0.5, f, 0.8, 0))
+        key = cv2.waitKey(300) & 0xFF
         if key == ord('q'):
             break
         elif key == ord(' '):
